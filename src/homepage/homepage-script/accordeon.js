4a464f0da3841870df5930a30bd4132746e25286
Deniska,VOLKRAFT <volkraft9@gmail.com>
2024-08-18 12:24:29 +0300

    first commit

---
+++ src/homepage/homepage-script/accordeon.js
@@ -0,0 +1,45 @@
+// const btnTitle = document.querySelectorAll('[data-name="accordeon-title"]');
+
+// const { default: log } = require("video.js/dist/types/utils/log");
+
+// btnTitle.forEach((item) => {
+// item.addEventListener("click", (event) => {
+//     console.log(event);
+//     const accordItem = event.currentTarget.closest(
+//         "accord-point"
+//     );
+//     accordItem
+//         .querySelector(".accord-point-decrpit")
+//         .classList.toggle("hidden");
+//     event.currentTarget.classList.toggle(
+//         "choouse-us__accord-point-title--active"
+//     );
+// });
+// });
+
+// 1. найти список, отследить клик
+// 2. проверить клик является accord-point
+// 3. найти внутри target descrpit и класс
+
+const accordList = document.querySelector("#accordList");
+
+const motionAccord = (event) => {
+    const isListItem = event.target.closest("[data-item]");
+    if (isListItem) {
+        isListItem.classList.toggle("active");
+    }
+};
+
+accordList.addEventListener("click", motionAccord);
+
+const headerAccrList = document.querySelector("#headerAccrList");
+
+const headerMotionAccord = (event) => {
+    const isHeaderListItem = event.target.closest("[data-accrdeon]");
+    if (isHeaderListItem) {
+        console.log(isHeaderListItem);
+        isHeaderListItem.classList.toggle("active");
+    }
+};
+
+headerAccrList.addEventListener("click", headerMotionAccord);
---
+++ src/homepage/index-entry.js
@@ -0,0 +1,8 @@
+import "./index.html";
+import "../common/common-style/common-style.scss";
+import "./homepage-style/preview.scss";
+import "./homepage-style/about-us.scss";
+import "./homepage-style/services.scss";
+import "./homepage-style/choouse-us.scss";
+import "./homepage-script/accordeon.js";
+import "./homepage-style/get-start.scss";
