0fa584cb1463afc8b2d2660809c0da860f2075b1
Deniska,VOLKRAFT <volkraft9@gmail.com>
2024-08-25 12:29:08 +0300

    Feat: style for section testimonial

--- src/homepage/index-entry.js
+++ src/homepage/index-entry.js
@@ -1,9 +1,12 @@
 import "./index.html";
+
 import "../common/common-style/common-style.scss";
 import "./homepage-style/preview.scss";
 import "./homepage-style/about-us.scss";
 import "./homepage-style/services.scss";
 import "./homepage-style/choouse-us.scss";
-import "./homepage-script/accordeon.js";
 import "./homepage-style/get-start.scss";
+import "./homepage-style/testimonial.scss";
+
+import "./homepage-script/accordeon.js";
 import "../common/common-scripts/sublist.js";
