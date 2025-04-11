c7defb84d462c79df63213ecace2ee785c910f65
Denis <volkraft9@gmail.com>
2025-04-11 19:50:27 +0300

    feat: create data for slider

---
+++ src/detail/detail-script/data-slider.js
@@ -0,0 +1,212 @@
+const dataSlider = [
+    {
+        id: 0,
+        title: 'service',
+        subtitle: 'VR Development',
+        descriptionFirst:
+            'At our VR development service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our VR design process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our VR development service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our VR development services.',
+        list: {
+            listTitle: 'Our VR development services include:',
+            listItems: [
+                'Custom VR development',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+    {
+        id: 1,
+        title: 'service',
+        subtitle: 'VR Games',
+        descriptionFirst:
+            'At our VR games service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our VR design process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our VR games service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our VR games services.',
+        list: {
+            listTitle: 'Our VR games services include:',
+            listItems: [
+                'Custom VR games',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+                'VR development',
+                'VR marketing campaigns',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+    {
+        id: 2,
+        title: 'service',
+        subtitle: 'VR Events',
+        descriptionFirst:
+            'At our VR events service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our VR design process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our VR events service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our VR events services.',
+        list: {
+            listTitle: 'Our VR events services include:',
+            listItems: [
+                'Custom VR events',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+                'VR development',
+                'VR marketing campaigns',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+    {
+        id: 3,
+        title: 'service',
+        subtitle: 'VR Design',
+        descriptionFirst:
+            'At our VR design service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our VR design process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our VR design service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our VR design services.',
+        list: {
+            listTitle: 'Our VR design services include:',
+            listItems: [
+                'Custom VR design',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+                'VR development',
+                'VR marketing campaigns',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+    {
+        id: 4,
+        title: 'service',
+        subtitle: 'VR Consulting',
+        descriptionFirst:
+            'At our VR consulting service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our VR design process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our VR consulting service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our VR consulting services.',
+        list: {
+            listTitle: 'Our VR consulting services include:',
+            listItems: [
+                'Custom VR consulting',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+                'VR development',
+                'VR marketing campaigns',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+    {
+        id: 5,
+        title: 'service',
+        subtitle: 'VR Entertainment',
+        descriptionFirst:
+            'At our VR entertainment service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our VR entertainment process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our VR entertainment service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our VR entertainment services.',
+        list: {
+            listTitle: 'Our VR entertainment services include:',
+            listItems: [
+                'Custom VR entertainment',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+                'VR development',
+                'VR marketing campaigns',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+    {
+        id: 6,
+        title: 'service',
+        subtitle: 'Client Testimony',
+        descriptionFirst:
+            'At our client testimony service, we specialize in creating immersive and engaging virtual reality experiences that transport your audience to a whole new world. Our team of experienced designers and developers work together to create VR experiences that are not only visually stunning but also interactive and user-friendly.',
+        descriptionSecond:
+            "We take a collaborative approach to our client testimony process, working closely with you to understand your specific needs and goals. Whether you're looking to create a VR training program, a marketing campaign, or a game, we can help you bring your idealife.",
+        descriptionThird:
+            'We use the latest VR technologies and software to create immersive experiences that are fully customized to your brand and message. We understand that every project is unique, and we work closely with you to ensure that we create a VR experience that perfectly matches your vision.',
+        descriptionFourth:
+            'At our client testimony service, we are committed to delivering high-quality VR experiences that engage, educate, and inspire your audience. Contact us today to discuss how we can help bring your ideas to life with our client testimony services.',
+        list: {
+            listTitle: 'Our client testimony services include:',
+            listItems: [
+                'Custom client testimony',
+                'Interactive VR experiences',
+                'VR training programs',
+                'VR marketing campaigns',
+                'VR game design',
+                'VR animation',
+                'VR development',
+                'VR marketing campaigns',
+            ],
+        },
+        btn: {
+            link: 'https://www.google.com',
+            text: 'contact us',
+        },
+    },
+]
+
+export default dataSlider
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
                 about: resolve(__dirname, 'src/about-us/about-us.html'),
                 service: resolve(__dirname, 'src/service/service.html'),
                 price: resolve(__dirname, 'src/price/price.html'),
+                detail: resolve(__dirname, 'src/detail/detail.html')
                 // faq: resolve(__dirname, 'src/faq/faq.html'),
                 // tnc: resolve(__dirname, 'src/t&c/t&c.html'),
                 // policy: resolve(__dirname, 'src/policy/policy.html')
