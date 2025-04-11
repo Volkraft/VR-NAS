b8f2314129047641f6baa7081b196d0efdca374d
Denis <volkraft9@gmail.com>
2025-04-11 19:16:20 +0300

    feat: adaptive section exprience on service page

--- src/service/service-entry.js
+++ src/service/service-entry.js
@@ -2,5 +2,7 @@ import 'normalize.css'
 
 import '../common/common-style/common-style.scss'
 
+import '../common/common-style/services.scss'
+
 import '../common/common-scripts/header.js'
 import '../common/common-scripts/scrollList.js'
